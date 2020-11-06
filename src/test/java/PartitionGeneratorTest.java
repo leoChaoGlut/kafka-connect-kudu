007720b48d3113596b7a56e02c162fa339c55276
leo <leoiscoding@163.com>
2020-11-06 14:48:30 +0800

    1.TODO 异常邮件告警 2.增加add partition生成器

--- src/test/java/PartitionGeneratorTest.java
+++ src/test/java/PartitionGeneratorTest.java
@@ -1,28 +1,56 @@
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class PartitionGeneratorTest {
+    Statement statement;
+
+    @Before
+    public void before() throws SQLException {
+        String url = "jdbc:presto://etl01:10100/kudu";
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        Connection connection = DriverManager.getConnection(url, properties);
+        statement = connection.createStatement();
+    }
+
     @Test
     public void test() throws ParseException {
 //        CALL kudu.system.add_range_partition('schema', 'table', '{"lower": "2018-01-01", "upper": "2018-06-01"}')
-        final TimeUnit timeUnit = TimeUnit.MONTH;
-        final Date lowerDate = DateUtils.parseDate("2018-01-01", TimeUnit.patterns());
+        final TimeUnit timeUnit = TimeUnit.DAY;
+        final Date lowerDate = DateUtils.parseDate("2020-07-08", TimeUnit.patterns());
         final int amount = 1;
         if (amount < 0) {
             throw new RuntimeException("amount less than 0:" + amount);
         }
-        final int partitionCount = 10;
-        final String schema = "s1";
-        final String table = "t1";
-        Date prevDate = lowerDate, nextDate;
-        final List<String> procedures = new ArrayList<>(partitionCount);
+        final int partitionCount = Integer.MAX_VALUE;
+        final String schema = "ods_pay";
+        final String table = "nr_pos_payment_slip";
+        Date prevDate = lowerDate, nextDate, upperDate, now = new Date();
+
+        switch (timeUnit) {
+            case DAY:
+                upperDate = DateUtils.addDays(now, 30);
+                break;
+            case MONTH:
+                upperDate = DateUtils.addMonths(now, 2);
+                break;
+            case YEAR:
+                upperDate = DateUtils.addYears(now, 1);
+                break;
+            default:
+                throw new RuntimeException("not supported:" + timeUnit);
+        }
+
+        final List<String> procedures = new ArrayList<>();
         String json;
         for (int i = 0; i < partitionCount; i++) {
             switch (timeUnit) {
@@ -39,10 +67,26 @@ public class PartitionGeneratorTest {
                     throw new RuntimeException("not supported:" + timeUnit);
             }
             json = "{\"lower\": \"" + DateFormatUtils.format(prevDate, timeUnit.pattern) + "\", \"upper\": \"" + DateFormatUtils.format(nextDate, timeUnit.pattern) + "\"}";
-            procedures.add("CALL kudu.system.add_range_partition('" + schema + "', '" + table + "', '" + json + "')");
+            final String procedure = "CALL kudu.system.add_range_partition('" + schema + "', '" + table + "', '" + json + "');";
+            System.out.println(procedure);
+            procedures.add(procedure);
             prevDate = nextDate;
+            if (nextDate.after(upperDate)) {
+                break;
+            }
         }
-        System.out.println(String.join("\n", procedures));
+//        System.out.println(String.join("\n", procedures));
+
+//        procedures.forEach(this::execute);
+    }
+
+    private void execute(String sql) {
+        try {
+            System.out.println(sql);
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public enum TimeUnit {
